ba0bc39644f4b8d414962c3dba03c7d64941f994
Kaushik042001 <110186036+Kaushik042001@users.noreply.github.com>
2022-12-09 00:44:01 +0530

    addeddiv

--- summation.c
+++ summation.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a=5,b=4,c,d,m;
+    int a=5,b=4,c,s,m,d;
     c=a+b;
-    d=a-b;
+    s=a-b;
     m=a*b;
-    printf("Sum of a and b is: %d",c);
+    d=a/b;
+    printf("Sum of a and b is: %d",s);
     printf("Subtraction of a and b is: %d",d);
     printf("Multiplication of a and b is: %d",m);
+     printf("Division of a and b is: %d",d);
+    
 }
