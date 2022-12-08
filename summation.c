7dd82024f6efbcef464fbe03d014fec68883ae23
Kaushik042001 <110186036+Kaushik042001@users.noreply.github.com>
2022-12-09 00:29:59 +0530

    addedmult

--- summation.c
+++ summation.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int a=5,b=4,c,d;
+    int a=5,b=4,c,d,m;
     c=a+b;
     d=a-b;
+    m=a*b;
     printf("Sum of a and b is: %d",c);
     printf("Subtraction of a and b is: %d",d);
+    printf("Multiplication of a and b is: %d",m);
 }
